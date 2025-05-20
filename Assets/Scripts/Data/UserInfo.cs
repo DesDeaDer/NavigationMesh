1747cd1ceb80b2909e3a8b9b57dd6b4e740c4218
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 22:32:00 +0300

    Update UserInfo.cs

--- Assets/Scripts/Data/UserInfo.cs
+++ Assets/Scripts/Data/UserInfo.cs
@@ -1,31 +1,20 @@
 ﻿using System;
 using UnityEngine;
 
-public class UserInfo : MonoBehaviour
-{
+public class UserInfo : MonoBehaviour {
     public event Action<int> OnAddScore;
     public event Action OnChangeKillsCount;
 
     public int Score { get; private set; }
     public int KillsCount { get; private set; }
 
-    public void AddScore(int value)
-    {
+    public void AddScore(int value) {
         Score += value;
-
-        if (OnAddScore != null)
-        {
-            OnAddScore(value);
-        }
+        OnAddScore?.Invoke(value);
     }
 
-    public void KillEnemy()
-    {
+    public void KillEnemy() {
         ++KillsCount;
-
-        if (OnChangeKillsCount != null)
-        {
-            OnChangeKillsCount();
-        }
+        OnChangeKillsCount.Invoke();
     }
 }
