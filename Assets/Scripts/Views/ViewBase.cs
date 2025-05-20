9ffb50b29b6dc106ce6ec616dec836cded43e57d
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 22:40:50 +0300

    Update ViewBase.cs

--- Assets/Scripts/Views/ViewBase.cs
+++ Assets/Scripts/Views/ViewBase.cs
@@ -1,14 +1,9 @@
 ﻿using UnityEngine;
 
-public abstract class ViewBase : MonoBehaviour, IView
-{
+public abstract class ViewBase : MonoBehaviour, IView {
     public void Show()
-    {
-        gameObject.SetActive(true);
-    }
+        => gameObject.SetActive(true);
 
     public void Hide()
-    {
-        gameObject.SetActive(false);
-    }
+        => gameObject.SetActive(false);
 }
