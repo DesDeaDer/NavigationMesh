d2b42ef5fb4c689f3b68d2e8d7823b26ddd9f7cf
__full_ing_arr <34687549+DesDeaDer@users.noreply.github.com>
2025-05-20 22:36:09 +0300

    Update LevetTargetKillsCounter.cs

--- Assets/Scripts/LevelTargets/LevetTargetKillsCounter.cs
+++ Assets/Scripts/LevelTargets/LevetTargetKillsCounter.cs
@@ -1,51 +1,23 @@
 ﻿using UnityEngine;
 
-public class LevetTargetKillsCounter : MonoBehaviour, ITargetLevel
-{
+public class LevetTargetKillsCounter : MonoBehaviour, ITargetLevel {
     [SerializeField] private int _countKills;
     [SerializeField] private UserInfo _userInfo;
 
-    public int CountKills
-    {
-        get
-        {
-            return _countKills;
-        }
-    }
-
-    public UserInfo UserInfo
-    {
-        get
-        {
-            return _userInfo;
-        }
-    }
-
-    public bool IsCompleted
-    {
-        get
-        {
-            return KillsCurrent >= CountKills;
-        }
-    }
+    public int CountKills => _countKills;
+    public UserInfo UserInfo => _userInfo;
+    
+    public bool IsCompleted 
+        => KillsCurrent >= CountKills;
 
     public int KillsCurrent { get; private set; }
 
-    private void OnEnable()
-    {
-        UserInfo.OnChangeKillsCount += OnChangeKillsCountHandler;
-    }
+    void OnEnable()
+        => UserInfo.OnChangeKillsCount += OnChangeKillsCountHandler;
 
-    private void OnDisable()
-    {
-        if (UserInfo != null)
-        {
-            UserInfo.OnChangeKillsCount -= OnChangeKillsCountHandler;
-        }
-    }
+    void OnDisable()
+        => UserInfo?.OnChangeKillsCount -= OnChangeKillsCountHandler;
 
-    private void OnChangeKillsCountHandler()
-    {
-        ++KillsCurrent;
-    }
+    void OnChangeKillsCountHandler()
+        => ++KillsCurrent;
 }
